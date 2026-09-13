e3fbd9633a27876f35badb0a298bfdf15c5b3eeb
adityajagtap976-collab <adityajagtap976@gmail.com>
2026-09-13 12:34:15 +0530

    Adds item creation and update APIs
    Introduces a Pydantic BaseModel for 'Item' to define structured data.
    Implements a POST endpoint at '/items/' to create new items and a PUT endpoint at '/items/{item_id}' to update existing ones.
    Leverages Pydantic for automatic request body parsing and data validation, ensuring robust API interactions.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,3 +17,19 @@ def read_item(item_id: int, q: str = None):
     # 'item_id' comes from the path: /items/42
     # 'q' comes from the query string: ?q=python
     return {"item_id": item_id, "query": q}
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"item_name": item.name, "total_price": item.price * 1.2}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item_name": item.name, "updated_price": item.price}
